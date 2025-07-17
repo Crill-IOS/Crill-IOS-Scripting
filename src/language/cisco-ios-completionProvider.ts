50eb5a04ffb70256ad9c46a7194569dcd8425a42
1059-25-HTL3R <1059@htl.rennweg.at>
2025-07-17 12:05:57 +0200

    no Autocompletion (removed everything)

--- src/language/cisco-ios-completionProvider.ts
+++ src/language/cisco-ios-completionProvider.ts
@@ -1,4 +1,4 @@
-import { LangiumDocument, AstNode} from "langium";
+import { LangiumDocument} from "langium";
 import { DefaultCompletionProvider } from "langium/lsp";
 import { CompletionParams, CancellationToken, CompletionList, CompletionItem , CompletionItemKind} from "vscode-languageserver";
 import { CiscoIosServices } from "./cisco-ios-module.js";
@@ -16,52 +16,10 @@ export class CiscoIosCompletionProvider extends DefaultCompletionProvider {
 
 
     override async getCompletion(document: LangiumDocument, params: CompletionParams, _cancelToken?: CancellationToken): Promise<CompletionList | undefined> {
-        const root: AstNode = document.parseResult.value;
-        
- 
- 
+        this.services;
         return {
             isIncomplete: false,
-            items: this.getCompletions(root.$type ,this.services)
+            items: []
         }
-    }
-
-
-    getCompletions(type: string, services: CiscoIosServices): CompletionItem[]{
-
-    const index = services.shared.workspace.IndexManager;
-
-    const results: CompletionItem[] = [];
-
-    const elements = index.allElements(type);
-    for (const el of elements) {
-        results.push({
-            label: el.name,
-            kind: CompletionItemKind.Keyword,
-            detail: type
-        });
-    }
-
-    return results;
-    }
- 
-    
-    
-
-    // getMode(node: CstNode): string{
-        
-        
-    //         if(current.$type ==='Command'){
-    //             const command = current as Command;
-    //             if(command.commands?.$type === 'Configure_cmd'){
-    //                 const configure =command.commands as Configure_cmd;
-    //                 if(configure.options.option === 'terminal'){
-    //                     return 'configure'
-    //                 }
-    //             }
-    //         }
-    //         return 'enable';
-    //     }
-        
-    
+    }   
 }
