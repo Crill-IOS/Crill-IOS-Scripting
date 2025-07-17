4b029d4a34790580ef0cc7f642f7f4bcf86367aa
1059-25-HTL3R <1059@htl.rennweg.at>
2025-07-17 13:04:53 +0200

    completion mode zwischenstand

--- src/language/cisco-ios-completionProvider.ts
+++ src/language/cisco-ios-completionProvider.ts
@@ -1,10 +1,9 @@
-import { LangiumDocument} from "langium";
+import { AstNode, LangiumDocument} from "langium";
 import { DefaultCompletionProvider } from "langium/lsp";
-import { CompletionParams, CancellationToken, CompletionList, CompletionItem , CompletionItemKind} from "vscode-languageserver";
+import { CompletionParams, CancellationToken, CompletionList} from "vscode-languageserver";
 import { CiscoIosServices } from "./cisco-ios-module.js";
-//import { CstUtils } from "langium";
-// import { CompletionItem } from "vscode/vscode/vs/editor/common/languages";
-//import * as ast from './generated/ast.js';
+import { Script } from "./generated/ast.js";
+
 
 
 
@@ -17,9 +16,24 @@ export class CiscoIosCompletionProvider extends DefaultCompletionProvider {
 
     override async getCompletion(document: LangiumDocument, params: CompletionParams, _cancelToken?: CancellationToken): Promise<CompletionList | undefined> {
         this.services;
-        return {
-            isIncomplete: false,
-            items: []
-        }
-    }   
+        // return {
+        //     isIncomplete: false,
+        //     items: []
+        // }
+        const root: AstNode = document.parseResult.value;
+        this.modeAtPosition(root, params);
+
+
+        return super.getCompletion(document,params,_cancelToken);
+
+    }
+    
+    
+    private modeAtPosition(node: AstNode, params: CompletionParams){
+        const script = node as Script;
+        const lines = script.script.lines;
+
+        console.log("HERE____________________________________")
+        console.log(lines);
+    }
 }
