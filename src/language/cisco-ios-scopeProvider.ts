df916fcfeae860ee3f576b9e27c458a243fa938e
1059-25-HTL3R <1059@htl.rennweg.at>
2025-08-09 13:57:28 +0200

    crossrefrence für ping command zu ip_cmd_options IP_ADRESS -> geht nicht + cisco-ios-scope provider aufgeräumt

--- src/language/cisco-ios-scopeProvider.ts
+++ src/language/cisco-ios-scopeProvider.ts
@@ -1,11 +1,5 @@
 /**
- * ALLES CHAT GENERATED!!!!!!!!!!!!!!!!!!!!!!!!
- * Benjamin 15.07
- * PS: meines Wissens wird dieser code von nichts benutzt und macht auch irgendwie nix
- * 
- * 
  * angeblich werden scopes standardmaessing nuer für das resolven von crossrefrences verwendet und sonnt wird er nicht aufgerufen
- * 
  * ^ bestätigt (getestet)
  * FIXEN
  */
@@ -13,55 +7,17 @@
 
 
 import {
-    AstNode,
-    AstNodeDescription,
     DefaultScopeProvider,
     ReferenceInfo,
     Scope,
-    StreamScope,
-    stream
 } from "langium";
 
 export class CiscoIosScopeProvider extends DefaultScopeProvider {
 
     override getScope(context: ReferenceInfo): Scope {
         console.log("getting scope!")
-        const node = context.container;
-        const mode = this.detectMode(node);
-
-        if (mode == 'configure terminal') {
-            return new StreamScope(this.getConfigureTerminalCommands());
-        }
-
         return super.getScope(context);
     }
 
-    private detectMode(node: AstNode): string | undefined {
-        let current: AstNode | undefined = node;
-        while (current) {
-            if (current.$type === 'Configure_cmd') {
-                const options = (current as any).options?.option;
-                if (options === 'terminal') {
-                    console.log("works");
-                    return 'configure terminal';
-                }
-            }
-            current = current.$container;
-        }
-        return undefined;
-    }
-
-    private getConfigureTerminalCommands() {
-        const result: AstNodeDescription[] = [];
-        const types = ['Hostname_cmd', 'Interface_cmd'];
-
-        for (const type of types) {
-            const elements = this.indexManager.allElements(type);
-            for (const item of elements) {
-                result.push(item);
-            }
-        }
-
-        return stream(result);
-    }
+    
 }
