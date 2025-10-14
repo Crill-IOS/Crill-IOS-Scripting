1f03c71dab8e69b61b0fdd78db0cf80cb1b1a295
1059-25-HTL3R <1059@htl.rennweg.at>
2025-10-14 15:22:50 +0200

    reformatting + doc's for check_double_ips_Interface

--- src/language/cisco-ios-validator.ts
+++ src/language/cisco-ios-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import { BANNER_MESSAGE, CiscoIosAstType , COMMENT, IP, IP_cmd_interface, isIP_cmd_interface, isIp_cmd_option_address, Stat, SUBNETMASK, Username_cmd,} from './generated/ast.js';
+import { BANNER_MESSAGE, CiscoIosAstType, COMMENT, IP, IP_cmd_interface, isIP_cmd_interface, isIp_cmd_option_address, Stat, SUBNETMASK, Username_cmd, } from './generated/ast.js';
 import type { CiscoIosServices } from './cisco-ios-module.js';
 import { AstUtils } from 'langium';
 
@@ -17,7 +17,7 @@ export function registerValidationChecks(services: CiscoIosServices) {
         Username_cmd: validator.checkUsername_cmd,
         BANNER_MESSAGE: validator.checkBANNER_MESSAGE,
         COMMENT: validator.checkCOMMENT,
-        Stat: validator.check_for_double_IPs,
+        Stat: validator.check_Stat,
 
     };
     registry.register(checks, validator);
@@ -28,113 +28,120 @@ export function registerValidationChecks(services: CiscoIosServices) {
  */
 export class CiscoIosValidator {
 
-    checkIP(ip: IP, accept:ValidationAcceptor): void {
-        if(ip.value){
-            const  splitIP= String(ip.value).split('.');
+    checkIP(ip: IP, accept: ValidationAcceptor): void {
+        if (ip.value) {
+            const splitIP = String(ip.value).split('.');
 
-            for ( const num of splitIP){
-                const ip_number = parseInt(num,10);
-                
-                if(ip_number > 255 || ip_number < 0 ){
-                    accept("error", "This is not a valid IP-Address", {node: ip, property: 'value'});
+            for (const num of splitIP) {
+                const ip_number = parseInt(num, 10);
+
+                if (ip_number > 255 || ip_number < 0) {
+                    accept("error", "This is not a valid IP-Address", { node: ip, property: 'value' });
                 }
             }
         }
     }
 
-    chekcSUBNETMASK(subnetmask:SUBNETMASK, accept:ValidationAcceptor):void{
-        if(subnetmask.value){   
+    chekcSUBNETMASK(subnetmask: SUBNETMASK, accept: ValidationAcceptor): void {
+        if (subnetmask.value) {
             const splitSUBNETMASK = String(subnetmask.value).split('.');
             let sub_binary: string = "";
-            for (const num of splitSUBNETMASK){
-                const sub_number = parseInt(num,10);
-                if (sub_number > 255){
+            for (const num of splitSUBNETMASK) {
+                const sub_number = parseInt(num, 10);
+                if (sub_number > 255) {
 
                 }
                 const sub_number_binary = sub_number.toString(2);
                 let temp = sub_number_binary;
-                for (let i = 0; i<(8-sub_number_binary.length); i++) {
+                for (let i = 0; i < (8 - sub_number_binary.length); i++) {
                     temp = "0" + temp;
                 }
-                sub_binary = sub_binary +temp;
+                sub_binary = sub_binary + temp;
             }
-            if (sub_binary.match(/10+1/) || sub_binary.length != 32 || !sub_binary.includes("0")){
-                accept("error", "This is not a valid Subnet-Mask", {node: subnetmask, property: 'value'});
+            if (sub_binary.match(/10+1/) || sub_binary.length != 32 || !sub_binary.includes("0")) {
+                accept("error", "This is not a valid Subnet-Mask", { node: subnetmask, property: 'value' });
             }
         }
     }
 
-    checkUsername_cmd(username_cmd: Username_cmd, accept: ValidationAcceptor):void {
-        if(username_cmd.options){
-            let occuredOptions:string[]= [];
-            for(const option of username_cmd.options){
-                if(occuredOptions.includes(option.$type)){
-                    accept("error", `Already Defined ${option.$cstNode?.text} (duplicate)`, {node:option});
-                }else {
+    checkUsername_cmd(username_cmd: Username_cmd, accept: ValidationAcceptor): void {
+        if (username_cmd.options) {
+            let occuredOptions: string[] = [];
+            for (const option of username_cmd.options) {
+                if (occuredOptions.includes(option.$type)) {
+                    accept("error", `Already Defined ${option.$cstNode?.text} (duplicate)`, { node: option });
+                } else {
                     occuredOptions.push(option.$type);
                 }
             }
         }
     }
-    checkBANNER_MESSAGE(BANNER_MESSAGE: BANNER_MESSAGE, accept: ValidationAcceptor):void {
+    checkBANNER_MESSAGE(BANNER_MESSAGE: BANNER_MESSAGE, accept: ValidationAcceptor): void {
         let message = BANNER_MESSAGE.message.join("");
         const delim1 = message.at(0);
-        const delim2 = message.charAt(message.length-1);
-        
-        if (delim1 != delim2){
-            accept("error", `Delimiters ${delim1} and ${delim2} dont match!`, {node:BANNER_MESSAGE});
-        }else if (message.substring(1,message.length -1 ).includes(delim1) || 
-                 (message.substring(1,message.length -1 ).includes(delim2))){
-            accept("error", `Delimiter (${delim1}) can not be inside MESSAGE`, {node:BANNER_MESSAGE});
+        const delim2 = message.charAt(message.length - 1);
+
+        if (delim1 != delim2) {
+            accept("error", `Delimiters ${delim1} and ${delim2} dont match!`, { node: BANNER_MESSAGE });
+        } else if (message.substring(1, message.length - 1).includes(delim1) ||
+            (message.substring(1, message.length - 1).includes(delim2))) {
+            accept("error", `Delimiter (${delim1}) can not be inside MESSAGE`, { node: BANNER_MESSAGE });
         }
     }
 
-    checkCOMMENT(COMMENT: COMMENT, accept: ValidationAcceptor):void {
-        if (COMMENT.delim ==  "!" || COMMENT.delim == "#"){
+    checkCOMMENT(COMMENT: COMMENT, accept: ValidationAcceptor): void {
+        if (COMMENT.delim == "!" || COMMENT.delim == "#") {
 
-        }else{
-            accept("error", `Delimiter (${COMMENT.delim}) can not be used to initialize a comment!`,{node:COMMENT, property: "delim"})
+        } else {
+            accept("error", `Delimiter (${COMMENT.delim}) can not be used to initialize a comment!`, { node: COMMENT, property: "delim" })
         }
     }
-    check_for_double_IPs(script: Stat, accept: ValidationAcceptor): void{
-        let ipHistory:string[] = [];
-        let commandHistory: IP_cmd_interface[] = [];
-        const addresses = 
-        AstUtils.streamAllContents(AstUtils.findRootNode(script))
-        .filter(e => e.$type == "IP_cmd_interface" )
-        
-        console.log(addresses + "address")
-        for (let address of addresses){
-            console.log("here")
-            if (isIP_cmd_interface(address) && isIp_cmd_option_address(address.option)){
-                const ip = address.option.ip?.value
-                if (ipHistory.includes(ip)){
-                    let pos =ipHistory.lastIndexOf(ip)
-                    accept("error", `Duplicate ip address ${ip}`,{node:address.option, property: "ip",})
-                    let old = commandHistory[pos];
-                    if (old){
-                        accept("error", `duplicate ip address ${ip}`, {node:old.option, property: "ip"})
+    check_Stat(script: Stat, accept: ValidationAcceptor): void {
+
+        //all checks that care about more than 1 node
+        check_double_ips_Interface(script);
+
+        //all check functions that care about more than 1 node
+        function check_double_ips_Interface(script: Stat) {
+            /**
+             * Checks for duplicate IP addresses within IP_cmd_interface nodes.
+             * Reports an error for both the first and subsequent occurrences.
+             * 
+             * @param script the root node of the ast
+             * 
+             * @example
+             * interface fastethernet 0/0
+             * ip address 1.2.3.4 255.255.255.0
+             * exit
+             *
+             * interface gigabitethernet 0/0
+             * ip address 1.2.3.4 255.255.255.0
+             * exit
+             * 
+             * ^ this "throws" an "error" since both ips are the same.
+             * the function does not care if it is the same or a diffrent interface.
+             */
+
+            const seenIPs = new Map<string, IP_cmd_interface>();
+
+            const addresses = AstUtils.streamAllContents(AstUtils.findRootNode(script))
+                .filter(e => e.$type === "IP_cmd_interface");
+
+            for (const address of addresses) {
+                if (isIP_cmd_interface(address) && isIp_cmd_option_address(address.option)) {
+                    const ip = address.option.ip?.value;
+                    if (!ip) continue;
+
+                    const existing = seenIPs.get(ip);
+                    if (existing) {
+                        // Duplicate found: report both
+                        accept("error", `Duplicate IP address: ${ip}`, { node: address.option, property: "ip" });
+                        accept("error", `Duplicate IP address: ${ip}`, { node: existing.option, property: "ip" });
+                    } else {
+                        seenIPs.set(ip, address);
                     }
-                    
-                }else {
-                    ipHistory.push(ip);
-                    commandHistory.push(address);
                 }
             }
-            
         }
     }
 }
-
-
-
-    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-    //     if (person.name) {
-    //         const firstChar = person.name.substring(0, 1);
-    //         if (firstChar.toUpperCase() !== firstChar) {
-    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-    //         }
-    //     }
-    // }
-
-
