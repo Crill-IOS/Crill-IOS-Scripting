8a62c275c95b29468ee1bf66a1d9affdfef4c271
Fabian Ha <1182@htl.rennweg.at>
2025-10-19 20:03:15 +0200

    Implemented highlighting for Grundkonfiguration except for missing terminals

--- src/language/cisco-ios-semanticTokenProvider.ts
+++ src/language/cisco-ios-semanticTokenProvider.ts
@@ -1,43 +1,43 @@
 import { SemanticTokenAcceptor, AbstractSemanticTokenProvider } from 'langium/lsp';
 import { AstNode } from 'langium';
+import { isInterface_types } from './generated/ast.js';
 
 const TOKEN_MAP: Record<string, { type: string; modifier?: string; keyword?: string }> = {
-    Comment: { type: 'comment' },
-
-    Hostname_Input: { type: 'variable', modifier: 'declaration' },
-    USERNAME_INPUT: { type: 'string', modifier: 'declaration' },
-    BANNER_MESSAGE: { type: 'string' },
-    DOMAINNAME_INPUT: { type: 'variable' },
-    INPUT: { type: 'variable' },
-
-    INT: { type: 'number' },
-    VERSION_INPUT: { type: 'number' },
-    MODULUS_INPUT: { type: 'number' },
-
-    IP: { type: 'variable', modifier: 'readonly' },
-    SUBNETMASK: { type: 'variable', modifier: 'readonly' },
+    // Grundkonfig
+    HOSTNAME_INPUT: { type: 'number' },
+    USERNAME_INPUT: { type: 'number'},
+    BANNER_MESSAGE: { type: 'number' },
+    DOMAINNAME_INPUT: { type: 'number' },
     Interface_number: { type: 'number'},
+    VERSION_INPUT: { type: 'number'},
+    MODULUS_INPUT: { type: 'number'},
+    USERNAME_PASSWORD_INPUT: { type: 'number'},
+    IP: { type: 'number'},
+    SUBNETMASK: { type: 'number'},
 
-    Username_cmd_option: { type: 'variable', modifier: 'declaration' },
+    // Allgemein
+    COMMENT: { type: 'comment'},
+
+    // zukünftig
+    OSPF_PROCESS_NUMBER: { type: 'number'},
+
 };
 
 export class CiscoIosSemanticTokenProvider extends AbstractSemanticTokenProvider {
     protected override highlightElement(node: AstNode, acceptor: SemanticTokenAcceptor): void {
+        if (isInterface_types(node)) {
+            acceptor({
+                node,
+                property: 'type',
+                type: 'variable',
+            })
+            return
+        }
+
         const mapping = TOKEN_MAP[node.$type];
         if (!mapping) return;
-
-        if (mapping.type === 'keyword') {
-            const keywordText = mapping.keyword ?? node.$cstNode?.container?.content?.[0]?.text;
-            if (keywordText) {
-                acceptor({ node, keyword: keywordText, type: 'keyword' });
-            }
-        } else if (mapping.type === 'function') {
-            const firstToken = node.$cstNode?.container?.content?.[0];
-            if (firstToken) {
-                acceptor({ cst: firstToken, type: 'function', modifier: mapping.modifier });
-            }
-        } else if (node.$cstNode) {
-            acceptor({ cst: node.$cstNode, type: mapping.type, modifier: mapping.modifier });
+        if (node.$cstNode) {
+            acceptor({ cst: node.$cstNode, type: mapping.type})
         }
     }
 
